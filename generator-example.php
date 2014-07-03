a0d37f6ab5fca91485bd9fbbd48be24ca8e4ad64
Dennis Heckman <dennis.heckman@vertive.com>
2014-07-03 17:43:33 -0500

    update generator example file

--- generator-example.php
+++ generator-example.php
@@ -11,7 +11,7 @@ class ModelGenerator extends \Phrame\Generator
     {
         // used to ask the user a question on the command line
         // parameter 1: what the user will see on the command line
-        // parameter 2: property to store response on ModelGenerator
+        // parameter 2: key to access config option via "$this->config('username')"
         // parameter 3 (optional): default if user doesn't answer
         $this->prompt("What is your name?", "username", "beelzebub");
         $this->prompt("Where are you from?", "place");
@@ -34,7 +34,21 @@ class ModelGenerator extends \Phrame\Generator
         // run another generator
         $this->runGenerator('OtherGenerator');
 
+        // returns path to destination, usually the current working directory
+        $destPath = $this->getDestination();
+
+        // returns path to source, usually the directory this file can be found in
+        $sourcePath = $this->getSource();
+
         // TODO: examples for generating files from a template
+
+        // set default configuration options using a config file
+        // will look in $this->getSource()
+        // (prompt method will override config options)
+        $this->setConfig('config-file.json');
+
+        // access configuration options via
+        $face = $this->config('your-face');
     }
 }
 
@@ -47,4 +61,6 @@ class ModelGenerator extends \Phrame\Generator
 /**
  * TODO:
  * 1. Switch to PSR-4 autoloading
+ * 2. Implement a generator generator (Yo Dawg...)
+ * 3. Implement a generator test generator
  */
