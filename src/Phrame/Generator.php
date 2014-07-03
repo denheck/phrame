58f9458ecd8f9debb293b6d4ae7133d007a351ba
Dennis Heckman <dennis.heckman@vertive.com>
2014-07-03 17:44:00 -0500

    add setConfig method to Phrame\Generator

--- src/Phrame/Generator.php
+++ src/Phrame/Generator.php
@@ -1,6 +1,18 @@
 <?php
 
+namespace Phrame;
+
 class Generator
 {
+    private $config;
 
+    /**
+     * @param string $configFile JSON configuration file's name
+     */
+    public function setConfig($configFile)
+    {
+        $source = $this->getSource();
+        // TODO: handle erroneous JSON
+        $this->config = json_decode($configFile);
+    }
 }
